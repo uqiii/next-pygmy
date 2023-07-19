2d5f982de94edda17aaae38d8674b848d5ae9b94
Uqi <musimhujan230@gmail.com>
2023-07-19 18:55:31 +0700

    add logo to head

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,6 @@ import '@styles/globals.css'
 
 import Provider from '@components/Provider'
 import Nav from '@components/Nav'
-
 export const metadata = {
   title: "Pygmy",
   description: "A Website by Uqi"
@@ -11,6 +10,10 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
+      <head>
+        <link rel="icon" href="/assets/images/logo.png" />
+      </head>
+
       <body>
         <Provider>
           <div className="main">
