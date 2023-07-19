8f9490a5c3c3d6a011d33bea187f6393762195b7
Uqi <musimhujan230@gmail.com>
2023-07-19 18:18:52 +0700

    add greeting

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
+"use client"
+
+import { useSession } from "next-auth/react";
+
 const Home = () => {
+  const { data: session } = useSession();
+
+  const renderGreeting = () => (
+    <p className="text-center">
+      {`Hi, ${session.user.name}!`}
+    </p>
+  );
+
   return (
     <section className="w-full flex-center flex-col">
+      {session?.user && renderGreeting()}
       <h1 className="head_text text-center">
         Pygmy
       </h1>
